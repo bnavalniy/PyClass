f4c5d018e8a026a218737db56deb2c2768036bf5
bogdan.navalnyi <bogdan.navalnyi@4finance.com>
2020-05-31 13:23:48 +0300

    2 Task switch

---
+++ homeWorks/2/Task2SwtichElements.py
@@ -0,0 +1,15 @@
+my_list = list(input("Fill the list: "))
+buffer_list = []
+switched_copy_of_my_list = []
+for ind, el in enumerate(my_list):
+    if ind % 2 == 0 and ind != 0:
+        buffer_list.reverse()
+        switched_copy_of_my_list.extend(buffer_list)
+        buffer_list.clear()
+    buffer_list.append(el)
+    if ind == len(my_list) - 1:
+        buffer_list.reverse()
+        switched_copy_of_my_list.extend(buffer_list)
+        my_list.clear()
+        my_list = switched_copy_of_my_list
+print(my_list)
